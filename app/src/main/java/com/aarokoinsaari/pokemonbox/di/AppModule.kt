6195519993167ef6f3c50a723017738077cce3a0
Aaro Koinsaari <aaro.koinsaari@proton.me>
2024-10-29 15:35:40 +0100

    Add necessary dependencies, create singles
    Adds PokeApi, Koin and Retrofit and OkHttp dependencies.

---
+++ app/src/main/java/com/aarokoinsaari/pokemonbox/di/AppModule.kt
@@ -0,0 +1,25 @@
+package com.aarokoinsaari.pokemonbox.di
+
+import okhttp3.OkHttpClient
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+val appModule = module {
+    single {
+        OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .build()
+    }
+
+    single {
+        Retrofit.Builder()
+            .baseUrl("https://pokeapi.co/api/v2/")
+            .client(get<OkHttpClient>())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
